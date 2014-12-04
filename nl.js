d68905362d70e7a849efb72c132887b78f51c647
Matus Kmit <matus.kmit@zhdk.ch>
2014-12-04 17:32:19 +0100

    getLilaIndex()

--- nl.js
+++ nl.js
@@ -1,4 +1,35 @@
-function getLilaIndex() {
+timeIntervals = [
+  [[3, 36], [6, 0]],
+  [[6, 0], [8, 24]],
+  [[8, 24], [10, 48]],
+  [[10, 48], [15, 36]],
+  [[15, 36], [18, 0]],
+  [[18, 0], [20, 24]],
+  [[20, 24], [22, 48]],
+  [[22, 48], [3, 36]]
+]
+
+getDayTimeInMinutes = function (hs, ms) {
+  return hs * 60 + ms;
+}
+
+
+getLilaIndex = function () {
   d = new Date;
-  return d.toString();
+  hs = d.getHours();
+  ms = d.getMinutes();
+  timeNowInMinutes = getDayTimeInMinutes(hs, ms);
+
+  var index = 0
+  for (var i = index; i < timeIntervals.length - 1; i++) {
+    hs = timeIntervals[i][0][0]
+    ms = timeIntervals[i][0][1]
+
+    if ( timeNowInMinutes < getDayTimeInMinutes(hs, ms) ) {
+      index = i;
+      break;
+    }
+  }
+
+  return index;
 }
