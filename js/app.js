1f29330a3dbacc424e08b1323bd1d61a15860c06
Matus Kmit <matus.kmit@zhdk.ch>
2015-05-09 16:10:40 +0200

    release v1.0.0
    based on
    https://github.com/IronSummitMedia/startbootstrap-one-page-wonder
    
    Squashed commit of the following:
    
    commit fb7638e9c0c9cbb0fcda5abf7d9e647876a774a3
    Author: Matus Kmit <matus.kmit@zhdk.ch>
    Date:   Sat May 9 16:08:42 2015 +0200
    
        auto hide navbar
    
    commit 28700247eb768258787a0ea7713aba3061afb55f
    Author: Matus Kmit <matus.kmit@zhdk.ch>
    Date:   Fri May 8 19:50:32 2015 +0200
    
        bootstrap anchor
    
    commit 1b633380403bf54463755392ca384c4d5f2b174b
    Author: Matus Kmit <matus.kmit@zhdk.ch>
    Date:   Tue May 5 23:07:28 2015 +0200
    
        copyright & enable 3rd party js
    
    commit b150b861faad308069506620ea2b4e628eadb0be
    Author: Matus Kmit <matus.kmit@zhdk.ch>
    Date:   Tue May 5 17:07:16 2015 +0200
    
        header image & opacity
    
    commit c515a993da21ac1df702484c39bcdd1d82743fa5
    Author: Matus Kmit <matus.kmit@zhdk.ch>
    Date:   Tue May 5 00:20:26 2015 +0200
    
        copy over files & adjust sections & images

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 'use strict';
 
 var update = require('./update').update;
+var jQuery = require('jquery');
+window.$ = jQuery;
+window.jQuery = jQuery;
 
-global.window.addEventListener('load', function() {
+// this variable is not used -> always global
+require('bootstrap');
+require('bootstrap-autohidingnavbar/dist/jquery.bootstrap-autohidingnavbar');
+
+window.addEventListener('load', function() {
+  $('.navbar-fixed-top').autoHidingNavbar();
   update();
   setInterval(update, 1000);
+});
+
+window.addEventListener('hashchange', function(e) {
+  if (!window.location.hash == '') {
+    $('.navbar-fixed-top').autoHidingNavbar('hide');
+  }
 });
