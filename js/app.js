7c1d82c2a59090d35372914f252aa3d6a44f39cc
Matus Kmit <matus.kmit@zhdk.ch>
2015-05-10 19:21:17 +0200

    static navbar, no links

--- js/app.js
+++ js/app.js
@@ -7,16 +7,8 @@ window.jQuery = jQuery;
 
 // this variable is not used -> always global
 require('bootstrap');
-require('bootstrap-autohidingnavbar/dist/jquery.bootstrap-autohidingnavbar');
 
 window.addEventListener('load', function() {
-  $('.navbar-fixed-top').autoHidingNavbar();
   update();
   setInterval(update, 1000);
-});
-
-window.addEventListener('hashchange', function(e) {
-  if (!window.location.hash == '') {
-    $('.navbar-fixed-top').autoHidingNavbar('hide');
-  }
 });
